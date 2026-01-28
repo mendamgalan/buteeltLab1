122d9b0d76e5c3ff9e30762373f61b565fbfc25e
unknown <mendamgalanjargal@gmail.com>
2026-01-28 17:57:01 +0800

    changes

--- example_artifact/src/main/java/group/App.java
+++ example_artifact/src/main/java/group/App.java
@@ -6,5 +6,18 @@ package group;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String name = "Alice";
+        int age = 25;
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+        System.out.println("\nCounting to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
     }
 }
